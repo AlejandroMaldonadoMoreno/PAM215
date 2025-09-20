719cac61c2fd8ba63e608cd70744d438deff7ac8
AlejandroMaldonadoMoreno <124050449@upq.edu.mx>
2025-09-19 20:13:34 -0600

    Cambio de var a let o const
    Se cambiaron los var por let o const de ser necesario.

---
+++ Practica1/LetConst.js
@@ -0,0 +1,7 @@
+let nombre = "Armando"; //se cambio a let porque se redefine el valor
+const edad = 25; // se cambio a const porque no se altero el valor de edad
+
+nombre = "Ana Maria";//Es el nuevo valor de la variable nombre
+
+const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";//se cambio a const porque no se esta alterando el valor de saludo
+console.log(saludo);
