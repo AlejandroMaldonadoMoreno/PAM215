d00ab77de5cb678767afa834748a5810f6a54b46
AlejandroMaldonadoMoreno <124050449@upq.edu.mx>
2025-09-19 20:26:03 -0600

    Se creo funcion flecha para el cuadrado de un numero

---
+++ Practica1/FuncionArrow.js
@@ -0,0 +1,4 @@
+const cuadrado = (num)=> num * num;
+//Se definio una funcion tipo flecha que recibe un numero y devuelve el cuadrado de es
+//usamos num como "parametro" para poder ingresar un numero desde la consola
+console.log(cuadrado(25));
