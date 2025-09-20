b502ec79a115bf1aa24286e01bb70fd64e67ccd8
AlejandroMaldonadoMoreno <124050449@upq.edu.mx>
2025-09-19 20:32:45 -0600

    Se creo una funcion arrow con saludo personalizado

---
+++ Practica1/SaludoPersonalizado.js
@@ -0,0 +1,6 @@
+const saludo = (nombre, edad) => {
+    return "Hola, me llamo " + nombre + " y tengo " + edad + " años.";
+}
+//se creo una funcion de fleca con parametros nombre y edad para porteriormente
+//Agregarlos al saludo personalizado de la consola
+console.log(saludo("Isay", 37));
