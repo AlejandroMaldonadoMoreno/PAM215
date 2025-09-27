d35838dbb015850823191fb82d9144965e9695b3
AlejandroMaldonadoMoreno <124050449@upq.edu.mx>
2025-09-26 20:21:34 -0600

    Metodo main donde se muestra en la consola los resultado

---
+++ Practica3/main.js
@@ -0,0 +1,7 @@
+import restar from './util.js';
+//se usa la palabra import para traer la funcion restar del archivo util.js
+
+console.log(restar(5,2));
+console.log(restar(5,3));
+console.log(restar(10,4));
+//con los console log es para asignar valores a la funcion restar y mostrar el resultado en la consola.
