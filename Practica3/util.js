a5e458bf7b0b3af409849d9c190eed8298a0a625
AlejandroMaldonadoMoreno <124050449@upq.edu.mx>
2025-09-26 20:21:53 -0600

    Archivo con la funcion y el export

---
+++ Practica3/util.js
@@ -0,0 +1,7 @@
+export default function restar(a,b){
+    return a-b;
+}
+//Se uso el metodo export default para
+// exportar la funcion restar y poder usarla en el archivo main.js.
+//Y se uso el default para indicar que es la exportacion por defecto de la funcion.
+
