1ee74b935155ec32ac576f53f6c4a5f40fcb31f3
AlejandroMaldonadoMoreno <124050449@upq.edu.mx>
2025-10-10 20:16:14 -0600

    Se crearon botones y sus parametros de la nueva view

--- intro/App.js
+++ intro/App.js
@@ -1,19 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+//1. imports: Zonas de importaciones
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import React, {useState} from 'react';
+
+//2. Main: Zona de componentes
 export default function App() {
+  const[contador, setContador]=useState(0);
+
   return (
+
     <View style={styles.container}>
-      <Text>Hola mundo React Native!!</Text>
+      <Text>Contador: {contador}</Text>
+        <Button title={'Incrementar'} onPress={()=>setContador(contador+1)}> </Button>
+        <Button title={'Reiniciar'} onPress={()=>setContador(contador-contador)}></Button>
+        <Button title={'Decrementar'} onPress={()=>setContador(contador-1)}></Button>
+
       <StatusBar style="auto" />
     </View>
+
+
   );
 }
 
+//3. Estilos: Zona de los estilos y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
