bbd23b64e7eb063eec9403c49be4ff9f84b4c533
AlejandroMaldonadoMoreno <124050449@upq.edu.mx>
2025-10-17 20:17:29 -0600

    Buttons y Switch Completada

--- intro/Screens/BotonesScreen.js
+++ intro/Screens/BotonesScreen.js
@@ -1,21 +1,48 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Switch, Button} from 'react-native';
 
-export default function  BotonesScreen() {
-  return (
-    <View style={styles.container}>
-     <Text style={styles.text}> Proximamente por Daniel JR</Text>
-    </View>
-  );
-};
+export default function BotonesScreen() {
+    const [esEncendido, cambiarEncendido] = useState(false);
+    const [color, cambiarColor] = useState('yellow');
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Control de Luz</Text>
+            {/* Controlador ternario */}
+            <Text style={[styles.text, { color: esEncendido ? color : 'black' }]}>
+                {esEncendido ? 'Luz encendida' : 'Luz apagada'}
+            </Text>
+            <Switch
+                value={esEncendido}
+                onValueChange={() => cambiarEncendido(!esEncendido)}
+                trackColor={{ false: 'gray', true: 'yellow' }}
+            />
+
+            <View style={styles.cajaBotones}>
+                <Button title={'Amarillo'} color={'#b9a100'}  onPress={() => esEncendido && cambiarColor('yellow')} />
+                <Button title={'Azul'} color={'blue'}  onPress={() =>esEncendido && cambiarColor('blue')} />
+                <Button title={'Rojo'} color={'red'} onPress={() =>esEncendido && cambiarColor('red')} />
+            </View>
+        </View>
+    );
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f0f0f0',
-  },
-
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#5d5d5d',
+    },
+    text: {
+        fontSize: 30,
+        marginBottom: 10,
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    cajaBotones: {
+       flexDirection: 'row',
+        gap: 10,
+        marginTop: 10,
+    },
 });
-
