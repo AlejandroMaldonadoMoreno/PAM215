c8da50d51d408611e6355f8893cd7eee1e1e20a5
AlejandroMaldonadoMoreno <124050449@upq.edu.mx>
2025-10-21 20:31:32 -0600

    ImageBackgroung & SlapshScreen: Completada

--- intro/Screens/ImageScreen.js
+++ intro/Screens/ImageScreen.js
@@ -1,21 +1,90 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { View, Text, StyleSheet, ImageBackground, Animated, Easing} from 'react-native';
 
 export default function  ImageScreen() {
+    const[cargando, setCargando]= useState(true);
+    const desvanecido=new Animated.Value(1);
+
+    useEffect(()=> {
+        const timer = setTimeout(() => {
+            Animated.timing(desvanecido, {
+                toValue:0,
+                duration: 800,
+                easing: Easing.out(Easing.ease),
+                useNativeDriver: true,
+            }).start(() => setCargando(false));
+
+        }, 2000);
+        return()=>clearTimeout(timer);
+    }, []);
+
+    if(cargando){
+     return(
+         <Animated.View
+         style={[styles.splashcontainer, {opacity: desvanecido}]}
+         >
+             <ImageBackground
+             source={require('../assets/img2.jpg')}
+             resizeMode='center'
+             style={styles.splashImage}
+             >
+
+                 <Text style={styles.splashText}> Cargando... </Text>
+
+             </ImageBackground>
+         </Animated.View>
+     );
+    }
+
+
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}> Proximamente por Carlos Alberto</Text>
-        </View>
+        <ImageBackground
+        source={require('../assets/img1.jpg')}
+        style={styles.background}
+        resizeMode='cover'
+        >
+            <Text style={styles.texto}>Bienvenid@ a mi app</Text>
+
+        </ImageBackground>
+        
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
+    background: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#f0f0f0',
+        width: '100%',
+        height: '100%',
     },
+    texto:{
+        color: 'white',
+        fontSize: 24,
+        fontweight: 'bold',
+    },
+    splashcontainer:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding:50,
+
+    },
+    splashImage:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        width:'100%',
+
+    },
+    splashText:{
+        position:'absolute',
+        marginBottom:60,
+        fontSize:20,
+        color:'black',
+
+    }
 
 });
 
