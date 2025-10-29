7b89b34900ce8b8f8f5eb0338768d818c6a9bf13
AlejandroMaldonadoMoreno <124050449@upq.edu.mx>
2025-10-28 20:28:29 -0600

    ActivityIndicator & List’s: Completada

--- intro/Screens/ActivityIndicatorScreen.js
+++ intro/Screens/ActivityIndicatorScreen.js
@@ -1,21 +1,68 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, Pressable, ActivityIndicator } from 'react-native';
 
 export default function  ActivityIndicatorScreen() {
-    return (
+    const [loading, setLoading] = useState(false);
+
+    const startLoading = () => {
+        setLoading(true);
+        setTimeout(() => setLoading(false), 3000);
+    };
+    if (loading) {
+        return(
         <View style={styles.container}>
-            <Text style={styles.text}> Proximamente por  Leonel Emiliano</Text>
+            <View >
+                <ActivityIndicator
+                    size="large"
+                    animating={true}
+                    color="blue"
+                    hidesWhenStopped={true}
+                />
+
+                <Text style={styles.texto}>Cargando...</Text>
+            </View>
         </View>
+
+        );
+
+    }
+    return (
+      <View style={styles.container}>
+         <Text style={styles.texto}>Actividad Indicator</Text>
+            <Pressable style={styles.boton} title="Cargar" onPress={startLoading} >
+                <Text style={styles.textoBoton}>Cargar</Text>
+            </Pressable>
+      </View>
     );
-};
+}
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#f0f0f0',
+        backgroundColor: '#80c4ff',
     },
+    texto:{
+        marginBottom: 20,
+        fontSize: 18,
+        fontWeight: '400',
+        color: '#000000',
+    },
+    boton:{
+        backgroundColor:'#002aff',
+        borderRadius:20,
+        width:'35%',
+        alignItems:'center',
+        justifyContent:'center',
+        elevation:5,
+    },
+    textoBoton:{
+        color:'white',
+        fontSize:18,
+        padding:10,
+    },
+
 
 });
 
