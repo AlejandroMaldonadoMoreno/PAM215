d764bcfbfe8921ce90eb88732656a8ff58330dd2
AlejandroMaldonadoMoreno <124050449@upq.edu.mx>
2025-11-07 20:30:53 -0600

    Ultimo commit

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,11 +1,31 @@
 import React, {useState, useEffect} from 'react';
-import { View, Text, StyleSheet, ScrollView, Easing, Animated, Button, ImageBackground } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Easing, Animated, Button, ImageBackground, Switch } from 'react-native';
 
 export default function App() {
 
-
     const[cargando, setCargando]=useState(true);
     const desvanecido = new Animated.Value(1);
+    const [esEncendido, cambiarEncendido] = useState(false);
+    const [esEncendido2, cambiarEncendido2] = useState(false);
+
+
+    const [esEncendido3, cambiarEncendido3] = useState(false);
+    const [esEncendido4, cambiarEncendido4] = useState(false);
+
+
+    const [esEncendido5, cambiarEncendido5] = useState(false);
+    const [esEncendido6, cambiarEncendido6] = useState(false);
+
+    const apagarSwitches = () => {
+        cambiarEncendido(false);
+        cambiarEncendido2(false);
+        cambiarEncendido3(false);
+        cambiarEncendido4(false);
+        cambiarEncendido5(false);
+        cambiarEncendido6(false);
+    };
+
+
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -18,6 +38,7 @@ export default function App() {
         },2000);
         return () => clearTimeout(timer);
     }, []);
+
 
     if(cargando){
         return(
@@ -36,10 +57,14 @@ export default function App() {
           style={styles.splashImage}
           resizeMode='cover'
       >
-          <View style={styles.contenedorTitulo}>
-                <Text style={styles.titulo}>Mis deberes</Text>
-              <Text style={styles.fecha}> Fecha: 07/11/2025</Text>
+          <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center', paddingHorizontal:20, marginTop:40, gap:50}}>
+              <View style={styles.contenedorTitulo}>
+                  <Text style={styles.titulo}>Mis deberes</Text>
+                  <Text style={styles.fecha}> Fecha: 07/11/2025</Text>
+              </View>
+              <Button title={'Reiniciar Tareas'} onPress={()=> apagarSwitches()}></Button>
           </View>
+
 
           <View style={styles.contScroll}>
               <Text style={styles.Tareas}>Limpieza</Text>
@@ -51,13 +76,97 @@ export default function App() {
               >
 
                   <View style={styles.elementos2}>
-                      <Text style={styles.texto}>Barrer cada cuarto</Text>
-
+                      <Text style={styles.texto}>Descripción: Barrer cada cuarto</Text>
+                      <Text style={styles.texto}>Prioridad: Alta</Text>
+                      <Text style={styles.texto}>Estatus: Pendiente</Text>
+                      <Switch
+                          value={esEncendido}
+                          onValueChange={() => cambiarEncendido(!esEncendido)}
+                          trackColor={{ false: 'gray', true: 'green' }}
+                      />
                   </View>
 
                   <View style={styles.elementos2}>
-                      <Text style={styles.texto}>Lavar la ropa</Text>
+                      <Text style={styles.texto}>Descripción: Barrer cada cuarto</Text>
+                      <Text style={styles.texto}>Prioridad: Alta</Text>
+                      <Text style={styles.texto}>Estatus: Pendiente</Text>
+                      <Switch
+                          value={esEncendido2}
+                          onValueChange={() => cambiarEncendido2(!esEncendido2)}
+                          trackColor={{ false: 'gray', true: 'green' }}
+                      />
                   </View>
+              </ScrollView>
+
+          </View>
+          <View style={styles.contScroll}>
+              <Text style={styles.Tareas}>Comida</Text>
+              <ScrollView
+                  style={styles.scrollhorizontal}
+                  horizontal={true}
+                  nestedScrollEnabled={true}
+                  showsHorizontalScrollIndicator={true}
+              >
+
+                  <View style={styles.elementos2}>
+                      <Text style={styles.texto}>Descripción: Barrer cada cuarto</Text>
+                      <Text style={styles.texto}>Prioridad: Alta</Text>
+                      <Text style={styles.texto}>Estatus: Pendiente</Text>
+                      <Switch
+                          value={esEncendido3}
+                          onValueChange={() => cambiarEncendido3(!esEncendido3)}
+                          trackColor={{ false: 'gray', true: 'green' }}
+                      />
+                  </View>
+
+                  <View style={styles.elementos2}>
+                      <Text style={styles.texto}>Descripción: Barrer cada cuarto</Text>
+                      <Text style={styles.texto}>Prioridad: Alta</Text>
+                      <Text style={styles.texto}>Estatus: Pendiente</Text>
+                      <Switch
+                          value={esEncendido4}
+                          onValueChange={() => cambiarEncendido4(!esEncendido4)}
+                          trackColor={{ false: 'gray', true: 'green' }}
+                      />
+                  </View>
+
+
+
+              </ScrollView>
+
+          </View>
+
+          <View style={styles.contScroll}>
+              <Text style={styles.Tareas}>Escuela</Text>
+              <ScrollView
+                  style={styles.scrollhorizontal}
+                  horizontal={true}
+                  nestedScrollEnabled={true}
+                  showsHorizontalScrollIndicator={true}
+              >
+
+                  <View style={styles.elementos2}>
+                      <Text style={styles.texto}>Descripción: Barrer cada cuarto</Text>
+                      <Text style={styles.texto}>Prioridad: Alta</Text>
+                      <Text style={styles.texto}>Estatus: Pendiente</Text>
+                      <Switch
+                          value={esEncendido5}
+                          onValueChange={() => cambiarEncendido5(!esEncendido5)}
+                          trackColor={{ false: 'gray', true: 'green' }}
+                      />
+                  </View>
+
+                  <View style={styles.elementos2}>
+                      <Text style={styles.texto}>Descripción: Barrer cada cuarto</Text>
+                      <Text style={styles.texto}>Prioridad: Alta</Text>
+                      <Text style={styles.texto}>Estatus: Pendiente</Text>
+                      <Switch
+                          value={esEncendido6}
+                          onValueChange={() => cambiarEncendido6(!esEncendido6)}
+                          trackColor={{ false: 'gray', true: 'green' }}
+                      />
+                  </View>
+
 
 
 
@@ -89,10 +198,10 @@ const styles = StyleSheet.create({
     contenedorTitulo:{
         backgroundColor:'rgba(255, 255, 255, 0.7)',
         padding:20,
-        whidth:'100%',
+        width:'60%',
         alignItems:'center',
         borderRadius:10,
-        marginTop:-290,
+        marginTop:-60,
     },
     titulo:{
         fontSize:32,
@@ -103,14 +212,14 @@ const styles = StyleSheet.create({
     contScroll:{
         backgroundColor:'rgba(255, 255, 255, 0.7)',
         padding:20,
-        whidth:'100%',
-        borderRadius:10,
+        width:'100%',
+        borderRadius:20,
         marginTop:20,
         alignItems:'center',
     },
     elementos2:{
-        width: 200,
-        height: 120,
+        width: 250,
+        height: 100,
         backgroundColor: 'rgb(153,176,255)',
         justifyContent: 'center',
         alignItems: 'center',
