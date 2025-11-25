7a536ea1b301069b6a7368bc851ec8b9d9560b45
AlejandroMaldonadoMoreno <124050449@upq.edu.mx>
2025-11-24 21:31:28 -0600

    Insert y Select funcionando

---
+++ userDB/controllers/UsuarioController.js
@@ -0,0 +1,73 @@
+//Comenzamos el controlador del usuario importando el modelo
+// y el servicio de usuario, después de crea la clase y el
+// constructor del controlador y se inicializa el servicio de la BD
+
+
+import {Usuario} from '../models/usuario';
+import DatabaseService from "../database/DatabaseService";
+
+export class UsuarioController{
+    constructor() {
+        this.listeners = [];
+    }
+//inicia el controlador con el Service
+    async initialize(){
+        await DatabaseService.initialize();
+    }
+
+    //Como segunda parte aquí preparamos el controlador para
+    // invoque al servicio de consulta cuando se le indiquen
+
+    async obtenerUsuarios(){
+        try{
+            const data = await DatabaseService.getAll();
+            return data.map(u => new Usuario(u.id, u.nombre, u.fechaCreacion));
+        }catch(error){
+            console.error('Error al obtener usuarios: ', error);
+            throw new Error('No se pudieron cargar los usuarios', error);
+        }
+    }
+
+    //Después preparamos la función de crear usuario la cual
+    // primero valida en el modelo que se cumplan las reglas ya
+    // cumplido inserta usando el servicio, notifica a los observadores
+    // y retorna
+
+    async crearUsuario(nombre){
+        try{
+            //1 Validar datos
+            Usuario.validar(nombre);
+            //2. Insertar en la BD
+           const nuevoUsuario = await DatabaseService.add(nombre.trim());
+           //3. Notificar a los observadores
+           this.notifyListeners();
+
+           //4. Retornar el nuevo creado
+            return new Usuario(
+                nuevoUsuario.id,
+                nuevoUsuario.nombre,
+                nuevoUsuario.fecha_creacion
+            );
+        }catch(error){
+            console.error('Error al crear usuario: ', error);
+            throw error;
+        }
+    }
+
+    //Por último, creamos el sistema de observadores que son los
+    // encargados de llevar el seguimiento y la actualización de la
+    // vista automática
+
+    //Sistema de observadores para actualizar la vista automáticamente
+    addListener(callback){
+        this.listeners.push(callback);
+    }
+
+    removeListener(callback){
+        this.listeners = this.listeners.filter(l => l !== callback);
+    }
+
+    notifyListeners(){
+        this.listeners.forEach(callback => callback());
+    }
+}
